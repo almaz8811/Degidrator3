9ec0c8b2213c5488d8bb418ae4689b4afdc0dcdd
almaz8811 <almaz8811@gmail.com>
2019-12-30 23:14:12 +0300

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266SSDP.h>
+#include <FS.h>
+#include <ArduinoJson.h>        //Установить из менеджера библиотек. https://arduinojson.org/
+#include <ESP8266HTTPUpdateServer.h>
+#include <DNSServer.h>
+#include <TickerScheduler.h>         //https://github.com/Toshik/TickerScheduler
+#include <DHT.h>                     //https://github.com/markruys/arduino-DHT   Support for DHT11 and DHT22/AM2302/RHT03
 
 void setup()
 {
